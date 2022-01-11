be36bacec1f6c384d1271a4d353724cc50f8988d
Jesper Petersen <info@ordklar.dk>
2022-01-11 10:03:00 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+function formatName(user) {
+  return user.firstName + " " + user.lastName;
+}
+
+const user = {
+  firstName: "Kasper",
+  lastName: "Petersen",
+};
 
 function App() {
+  const element = <b>Hej {formatName(user)}</b>;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {element} <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
